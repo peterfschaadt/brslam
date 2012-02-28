a6f3a19ff2802b1091b037910b80761bc7ae9e78
Peter Schaadt <peterfschaadt@yahoo.com>
2012-02-27 19:26:48 -0500

    Initial commit of Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brslam::Application.config.secret_token = 'bcf9ea2ec0b2c149a63598333411d2648f1bbaba067fc2ed6df1b1e7cf60af4c8d4beb2ec691f67c90abd6de85584fb64d58ef0e4811703321c4633da1a7c498'
