33a8a899b400504be22b473aa12dbb31bb068a73
Peter Schaadt <peterfschaadt@yahoo.com>
2012-02-27 20:27:44 -0500

    Added method to generate short code and to get ID.

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,13 @@
 class Url < ActiveRecord::Base
   validates :url, :presence => true
+
+  # Create a short code string
+  def short_code
+    id.to_s 36
+  end
+
+  # Take number from base 36 value to get ID
+  def self.find_using_short_code!(short_code)
+    find short_code.to_i(36)
+  end
 end
